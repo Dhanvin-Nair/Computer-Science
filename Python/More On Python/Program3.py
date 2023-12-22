e345054c0e1e6d26fd7ceaa897d151f7628038d3
Dhanvin-Nair <154566036+Dhanvin-Nair@users.noreply.github.com>
2023-12-22 06:36:53 +0530

    Create Program3.py

---
+++ Python/More On Python/Program3.py
@@ -0,0 +1,6 @@
+Radius = float(input("What is the radius of the circle: "))
+Circumference = 2*3.14*Radius
+Area = 3.14*Radius*Radius
+
+print('The circumference of the circle is: ', Circumference)
+print('The area of the circle is: ', Area)
