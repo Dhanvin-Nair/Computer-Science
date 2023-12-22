183d1923ce94df532e67bbeb4cf6a9fba8fea40a
Dhanvin-Nair <154566036+Dhanvin-Nair@users.noreply.github.com>
2023-12-22 06:33:50 +0530

    Create Program2.py

---
+++ Python/More On Python/Program2.py
@@ -0,0 +1,16 @@
+FirstValue = int(input("First Value: "))
+SecondValue = int(input("Second Value: "))
+
+Added = FirstValue+SecondValue
+Sub = FirstValue-SecondValue
+Multiplied=FirstValue*SecondValue
+Divided=FirstValue/SecondValue
+FloorDivided = FirstValue//SecondValue
+Cube=FirstValue**3
+
+print("The sum of the 1st number is: ", Added)
+print("The difference of the numbers are:", Sub)
+print("The product of the numbers is: ", Multiplied)
+print("The quotient of the numbers is: ", Divided)
+print("The result of the floor division of the numbers is: ", FloorDivided)
+print("The cube of the 1st number is: ", Cube)
