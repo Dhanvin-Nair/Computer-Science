e9f91fbbac700bc55f3e490bacbfbbe336a67cbf
Dhanvin-Nair <154566036+Dhanvin-Nair@users.noreply.github.com>
2024-01-11 06:25:45 +0530

    Create Program13.py

---
+++ Python/More On Python/Program13.py
@@ -0,0 +1,9 @@
+while True:
+    Number = int(input("Enter your number: "))
+
+    if Number>0:
+        print(f'The number {Number} is positive.')
+    elif Number<0:
+        print(f'The number {Number} is Negative.')
+    else:
+        print('The number is Zero.')
